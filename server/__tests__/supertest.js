41228a31ffd8d05615df4476d7840f81e7f8c3d5
jldiamond <root@LAPTOP-HBTDN1F7>
2024-01-23 11:28:49 -0500

    merging with supertest

---
+++ server/__tests__/supertest.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+
+const server = 'http://localhost:3000';
+
+describe('Route integration', () => {
+  describe('/', () => {
+    it('responds with 200 status and text/html content type', () => {
+      return request(server)
+        .get('/')
+        .expect('Content-Type', /text\/html/)
+        .expect(200);
+    });
+  });
+});
